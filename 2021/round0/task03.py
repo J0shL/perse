31e833f79a03532834154562a475a7a1bdf82ffd
J0shL <93035269+J0shL@users.noreply.github.com>
2023-04-06 13:18:11 +0100

    Create task03.py

---
+++ 2021/round0/task03.py
@@ -0,0 +1,7 @@
+chat = input().lower()
+if chat == "greet me":
+  print("hello")
+if chat == "compliment me":
+  print("you are kind")
+if chat == "discuss weather":
+  print("it is treacherous out there")
